d79f87b5404eccdf78f9fc0c6a6d21f126e33ec7
Vincent Foley <vfoley@gmail.com>
2013-12-17 09:59:47 -0500

    neighbors returns an empty set instead of Nothing

--- src/Digraph.hs
+++ src/Digraph.hs
@@ -18,11 +18,14 @@ empty = MkDigraph (M.empty)
 addEdge :: String -> String -> Digraph -> Digraph
 addEdge start end (MkDigraph m) =
     MkDigraph $ M.alter (\set -> case set of
-                       Nothing -> Just $ S.singleton end
-                       Just s  -> Just $ S.insert end s) start m
+                                   Nothing -> Just $ S.singleton end
+                                   Just s  -> Just $ S.insert end s) start m
 
-neighbors :: String -> Digraph -> Maybe (S.Set String)
-neighbors node (MkDigraph m) = M.lookup node m
+neighbors :: String -> Digraph -> S.Set String
+neighbors node (MkDigraph m) =
+    case M.lookup node m of
+      Nothing -> S.empty
+      Just s  -> s
 
 
 isAcyclic :: String -> Digraph -> Bool
@@ -34,6 +37,4 @@ isAcyclic startNode g@(MkDigraph m) = dfs (S.singleton startNode) S.empty
                   if curr `S.member` visited then
                       False
                   else
-                      case neighbors curr g of
-                        Nothing -> dfs toBeVisited' (S.insert curr visited)
-                        Just ns -> dfs (ns `S.union` toBeVisited') (S.insert curr visited)
+                      dfs (neighbors curr g `S.union` toBeVisited') (S.insert curr visited)
